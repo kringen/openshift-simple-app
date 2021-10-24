9c40d01f9563f311b8b0c68c8052c7a322628d65
Erik Kringen <ekringen@gmail.com>
2021-10-23 22:25:22 -0400

    Added host logging

--- nodejs/app.js
+++ nodejs/app.js
@@ -5,6 +5,7 @@ const os = require("os");
 http.createServer(function (request, response) {
 
    response.writeHead(200, {"Content-Type": "text/plain"});
+   console.log(request.headers["host"])
    
    response.end("Hello from " + os.hostname() + "!\n");
 }).listen(8080);
