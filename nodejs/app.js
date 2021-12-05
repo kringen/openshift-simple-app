5ec54c9b375cd3ecbbaee22041787952f6aa95cc
Erik Kringen <ekringen@gmail.com>
2021-12-04 23:36:45 -0500

    updating return value

--- nodejs/app.js
+++ nodejs/app.js
@@ -7,7 +7,7 @@ http.createServer(function (request, response) {
    response.writeHead(200, {"Content-Type": "text/plain"});
    console.log(request.headers["host"])
    
-   response.end("Hello from " + os.hostname() + "!\n");
+   response.end("Hello from " + os.hostname() + "!!\n");
 }).listen(8080);
 
 console.log('Server running at http://127.0.0.1:8080/');
